ac5c12f8307487432058b61e07822b1441146a26
Darcie Murphy <murphy-d72@ulster.ac.uk>
2025-06-28 18:48:47 +0100

    Exercise 1.13 completed

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -13,15 +13,23 @@ const App = () => {
   ]
    
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
 
   const handleNextAnecdote = () => {
     const randomIndex = Math.floor(Math.random() * anecdotes.length)
     setSelected(randomIndex)
   }
 
+   const handleVoteClick = () => {
+    const copy = [...votes]
+    copy[selected] += 1
+    setVotes(copy)
+  }
+
   return (
     <div>
-      {anecdotes[selected]}
+      <p>{anecdotes[selected]}</p>
+      <button onClick={handleVoteClick}>Vote</button>
       <button onClick={handleNextAnecdote}>
         Next Anecdote
       </button>
