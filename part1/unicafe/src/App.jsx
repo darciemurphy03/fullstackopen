84c5f87696f60d9eaa692cf9b51339c251911a9e
Darcie Murphy <murphy-d72@ulster.ac.uk>
2025-06-28 18:20:49 +0100

    Exercise 1.7 completed

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,6 +8,9 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const total = good + neutral + bad
+  const average = (good - bad) / total || 0
+  const positive = total === 0 ? 0 : (good / total) * 100 
 
   return (
     <div>
@@ -21,6 +24,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {average.toFixed(1)}</p>
+      <p>positive {positive.toFixed(1)} %</p>
     </div>
   )
 }
