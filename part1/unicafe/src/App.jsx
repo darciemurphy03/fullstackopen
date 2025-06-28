d1a14a6f258ba8d805ac719e582c1972bd66ebf9
Darcie Murphy <murphy-d72@ulster.ac.uk>
2025-06-28 18:22:57 +0100

    Exercise 1.8 completed

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,13 +4,28 @@ const Button = ({ text, onClick }) => (
   <button onClick={onClick}>{text}</button>
 )
 
+const Statistics = ({ good, neutral, bad }) => {
+  const total     = good + neutral + bad
+  const average   = total === 0 ? 0 : (good - bad) / total
+  const positiveP = total === 0 ? 0 : (good / total) * 100
+
+  return (
+    <div>
+      <h2>Statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {average.toFixed(1)}</p>
+      <p>positive {positiveP.toFixed(1)} %</p>
+    </div>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const total = good + neutral + bad
-  const average = (good - bad) / total || 0
-  const positive = total === 0 ? 0 : (good / total) * 100 
 
   return (
     <div>
@@ -20,13 +35,7 @@ const App = () => {
       <Button text="neutral" onClick={() => setNeutral(neutral + 1)} />
       <Button text="bad" onClick={() => setBad(bad + 1)} />
 
-      <h2>Statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {total}</p>
-      <p>average {average.toFixed(1)}</p>
-      <p>positive {positive.toFixed(1)} %</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
